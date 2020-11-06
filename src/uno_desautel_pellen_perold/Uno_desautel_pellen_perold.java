4245c29e08290416adddc3a4c374e09bb4c970f9
Quentin Dstl <qdesautel@gmail.com>
2020-11-06 10:45:18 +0100

    Creation projet

---
+++ src/uno_desautel_pellen_perold/Uno_desautel_pellen_perold.java
@@ -0,0 +1,9 @@
+package uno_desautel_pellen_perold;
+
+public class Uno_desautel_pellen_perold {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
