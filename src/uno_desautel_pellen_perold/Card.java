d770ffad4ac827c1a866a5d5698272214ce72973
clementpellen <clementpellen@gmail.com>
2020-11-06 14:34:25 +0100

    picks creation
    creation of pick_cards and trash

--- src/uno_desautel_pellen_perold/Card.java
+++ src/uno_desautel_pellen_perold/Card.java
@@ -2,7 +2,16 @@ package uno_desautel_pellen_perold;
 
 public interface Card {
 
-    public final static int NUMBER_TOTAL_CARD = 108;
+    public final static int NB_TOTAL_CARDS = 108;
+    
+    public static final int NB_COlOR = 4;
+    
+    public final static int NB_NUMBER_CARDS = 68;
+    public final static int NB_COLOR_CARDS = 19;
+    public final static int NB_COLOR_CARDS_EXCEPT_0 = 18;
+    public final static int NB_EACH_COLOR_NUMBER_EXCEPT_0 = 2;
+    public final static int NB_EACH_COLOR_0 = 1;
+    
     
     public boolean testUseCard ();
 
--- src/uno_desautel_pellen_perold/Game.java
+++ src/uno_desautel_pellen_perold/Game.java
@@ -1,14 +1,72 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package uno_desautel_pellen_perold;
 
-/**
- *
- * @author cleme
- */
+import java.util.ArrayList;
+import java.util.Random; 
+import java.util.Collections;
+
 public class Game {
     
+    private ArrayList<Card> m_pick_cards; //draw of cards
+    private ArrayList<Card> m_trash;      //trash of cards played
+    
+    
+    ///CONSTRUCTOR
+    public Game()
+    {
+        
+        m_pick_cards = new ArrayList<>();
+        m_trash = new ArrayList<>();
+    }
+    
+    ///INITIALISATION METHODS
+    public void initPickCards()
+    {
+        initNumberCards();
+        m_pick_cards = shufflePickCards(m_pick_cards);
+    }
+    
+    public void initNumberCards()
+    {
+        initNumberColorCards("blue");
+        initNumberColorCards("red");
+        initNumberColorCards("yellow");
+        initNumberColorCards("green");
+    }
+    
+    public void initNumberColorCards(String color)
+    {
+        m_pick_cards.add( new NumberCard(0, color));
+        for(int j=0; j<Card.NB_COLOR_CARDS_EXCEPT_0; ++j)
+        {
+            m_pick_cards.add( new NumberCard(j+1, color));   //creation of cards from 1 to 9
+        }
+        
+    }
+    
+    
+    ///SHUFFLE OF LIST METHOD
+    public ArrayList<Card> shufflePickCards(ArrayList<Card> pick_cards)
+    {
+        Random randomGenerator = new Random();  //declaration of an aleatory object
+        int randomInt; 
+        
+        boolean[] tab = new boolean[108];   //tab which checks eac indice of draw_cards
+        for(int i=0; i<tab.length; ++i)
+        {
+            tab[i] = false;
+        }
+        
+        for(int i=0; i<Card.NB_NUMBER_CARDS; ++i)
+        {
+            do 
+            {
+                randomInt = randomGenerator.nextInt(Card.NB_NUMBER_CARDS);
+            }while(tab[randomInt]!=false);
+            
+            Collections.swap(pick_cards, i, randomInt); //aleatory swap
+            tab[i] = true;  
+        }
+        
+        return pick_cards;
+    }
 }
