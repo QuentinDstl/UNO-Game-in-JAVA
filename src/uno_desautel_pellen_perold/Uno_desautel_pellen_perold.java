c0ecdde11b7ec6211061a2008954f7c38f8cc8b4
Charles P <c_perold@hotmail.fr>
2020-11-08 19:52:27 +0100

    Mise à jour interface graphique
    bug du main

--- src/uno_desautel_pellen_perold/Uno_desautel_pellen_perold.java
+++ src/uno_desautel_pellen_perold/Uno_desautel_pellen_perold.java
@@ -10,7 +10,24 @@ public class Uno_desautel_pellen_perold {
         
     }
     
-    public static void main(String[] args) {
+    public static void playWelcome()
+    {
+        int numberPlayer=0;
+        GraphicWelcome displayOne = new GraphicWelcome();
+        
+        do 
+        {            
+            numberPlayer = displayOne.getNumberPlayer();
+            System.out.print("");
+        } while (numberPlayer ==0);
+        
+        // A DELETE PLUS TARD 
+        System.out.println(" NUmbers players : "  +numberPlayer);
+    }
+    
+    public static void main(String[] args) 
+    {
+        playWelcome();
         
         playGame();
     }
